64fda1b218081dcbecc5481bb6b23cd60f9e73d2
Leonardo <leo@robol.it>
2010-06-15 09:55:53 +0200

    Aggiunto setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from distutils.core import setup
+
+setup(name="mlmanager",
+      version="0.1",
+      description="Python module to deliver mldonkey downloads",
+      author = "Leonardo Robol",
+      author_email = "leo@robol.it",
+      url = "http://poisson.phc.unipi.it/~robol/wordpress/programmi/mlmanager/",
+      py_modules = [ 'mlmanager' ])
+      
